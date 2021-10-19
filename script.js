ec7a904d558a8aa7ce2d9c3eeea2ee901d8afe71
Kevin Cenerino <cenerinok@gmail.com>
2021-10-19 09:55:21 +0200

    Adding all tailwind and icons

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const player1 = document.querySelector(".player1")
 const player2 = document.querySelector(".player2")
 const current1 = document.querySelector('.current1')
 const current2 = document.querySelector('.current2')
-const btn = document.getElementById("rollDice")
-const btnhold = document.getElementById('hold')
+const btn = document.querySelector(".rollDice")
+const btnhold = document.querySelector('.hold')
 const global1 = document.querySelector(".global1")
 const global2 = document.querySelector(".global2")
 const dice = document.querySelector(".dice")
+const newGame = document.querySelector(".newGame")
 
 
 
@@ -22,22 +23,22 @@ let total = 0;
 function imgDice(dicen) {
     switch(dicen){
         case 1:
-            dice.innerHTML = '<img src="media/1.png" alt="dice">';
+            dice.innerHTML = '<img  src="media/1.png" alt="dice">';
             break;
         case 2:
-            dice.innerHTML = '<img src="media/2.png" alt="dice">';
+            dice.innerHTML = '<img  src="media/2.png" alt="dice">';
             break;
         case 3:
-            dice.innerHTML = '<img src="media/3.png" alt="dice">';
+            dice.innerHTML = '<img  src="media/3.png" alt="dice">';
              break;
         case 4:
-            dice.innerHTML = '<img src="media/4.png" alt="dice">';
+            dice.innerHTML = '<img  src="media/4.png" alt="dice">';
             break;
         case 5:
             dice.innerHTML = '<img src="media/5.png" alt="dice">';
              break;
         case 6:
-            dice.innerHTML = '<img src="media/6.png" alt="dice">';
+            dice.innerHTML = '<img  src="media/6.png" alt="dice">';
              break;
         default:
             dice.innerHTML = "Le dé ne s'affiche pas";
@@ -53,17 +54,18 @@ function rolling(min, max) {
 
 
     if(result == 1){
-        total = -1
+        total = 0;
         changePlayer();
 
-    }
-    addition = current + result;
-    total += addition + current
-    if(gameStart){
-    current1.innerHTML = total
     }else{
-        current2.innerHTML = total
+        addition = current + result;
+        total += addition + current;
+            if(gameStart){
+                current1.innerHTML = total
+            }else{
+                current2.innerHTML = total
     }
+}
 
 }
 
@@ -72,43 +74,39 @@ function rolling(min, max) {
 function changePlayer(){
     if(gameStart) {
         gameStart = false
-        player2.classList.add('active')
-        player1.classList.remove('active')
-        current1.innerHTML = 0
+        player2.style.backgroundColor ="#a8e6cf";
+        player1.style.backgroundColor=""
+        player1Global += total;
+        global1.innerHTML = player1Global
+        total = 0;
+        current1.innerHTML = 0;
 
-    }else
-    {
+    }else{
         gameStart = true;
-        player1.classList.add('active')
-        player2.classList.remove('active')
-        current2.innerHTML = 0
+        player2.style.backgroundColor=""
+        player1.style.backgroundColor ="#a8e6cf";
+        player2Global += total;
+        global2.innerHTML = player2Global
+        total = 0;
+        current2.innerHTML = 0;
     };
 
 }
 
 
+newGame.addEventListener('click', ()=>{
+
+    location.reload() 
+})
+
 btn.addEventListener('click', ()=> {
     rolling(1,7);
     imgDice(result)
+    // const audio = new Audio('media/ONEDICE.WAV');
+    // audio.play();
 })
 
 btnhold.addEventListener('click', ()=>{
   changePlayer();
-    if(!gameStart){
-    player1Global += total;
-    global1.innerHTML = player1Global
-    total = 0;
-    current1.innerHTML = 0;
-  
-    }else
-    {
-        player2Global += total;
-        global2.innerHTML = player2Global
-        total = 0;
-        current2.innerHTML = 0;
-    }
-
 })
-
-
 
